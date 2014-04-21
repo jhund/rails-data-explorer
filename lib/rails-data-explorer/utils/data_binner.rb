b54cbd8c3f080d405a1da3e6dca15445f4e7d4b3
Jo Hund <jhund@clearcove.ca>
2014-04-21 14:32:24 -0700

    Added DataBinner

--- lib/rails-data-explorer/utils/data_binner.rb
+++ lib/rails-data-explorer/utils/data_binner.rb
@@ -6,3 +6,34 @@
 # * 21 - 30
 # * 31 - 40
 # * > 40
+class RailsDataExplorer
+  module Utils
+    class DataBinner
+
+      def initialize(*args)
+        @max = -Float::INFINITY
+        @bin_specs = [*args].compact.uniq.sort.map { |e|
+          case e
+          when Numeric
+            @max = [@max, e].max
+            { :label => "#{ e.to_s } or less", :lte => e }
+          else
+            raise "Handle this bin_spec: #{ e.inspect }"
+          end
+        }
+        @bin_specs << { :label => "> #{ @max }", :gt => @max }
+      end
+
+      def bin(value)
+        unless value.is_a?(Numeric)
+          raise(ArgumentError.new("Wrong type of value, numeric expected, got: #{ value.inspect }"))
+        end
+        bin = @bin_specs.detect { |bs|
+          (bs[:lte] && value <= bs[:lte]) || (bs[:gt] && value > bs[:gt])
+        }
+        bin[:label]
+      end
+
+    end
+  end
+end
---
+++ spec/rails-data-explorer/utils/data_binner_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../../helper_no_rails'
+
+class RailsDataExplorer
+  module Utils
+    describe DataBinner do
+
+      describe '#bin' do
+
+        [
+          [
+            '1',
+            [1,2,3],
+            [0,1,2,3,4],
+            ["1 or less", "1 or less", "2 or less", "3 or less", "> 3"]
+          ],
+        ].each do |(name, bin_specs, vals, xpect)|
+
+          it "bins #{ name }" do
+            db = DataBinner.new(*bin_specs)
+            vals.map{ |e| db.bin(e) }.must_equal xpect
+          end
+
+        end
+
+      end
+
+    end
+  end
+end
